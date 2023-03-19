e6566754f2e2ff60c1f850f5adee398e2add30be
Kevin Miguel Jiménez Hernández <hjmkalrevesconfecha@gmail.com>
2023-03-19 17:41:00 -0600

    map android fixed

--- App.js
+++ App.js
@@ -1,12 +1,9 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
 import LoginNavigation from './components/navigation/LoginNavigation';
-//import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    //<NavigationContainer>
       <LoginNavigation/>
-    //</NavigationContainer>
   );
 }
--- components/markers.js
+++ components/markers.js
@@ -13,19 +13,5 @@ export default[
         latitude:13.7151201,
         longitude:-89.1529652,
     },
-    {
-        index:3,
-        title:'Entrada Carretera de Oro UDB',
-        description:'',
-        latitude:13.714495,
-        longitude:-89.153755,
-    },
-    {
-        index:4,
-        title:'Entrada Ciudad Delgado UDB',
-        description:'',
-        latitude:13.717160,
-        longitude:-89.155880,
-    },
 ]
     
